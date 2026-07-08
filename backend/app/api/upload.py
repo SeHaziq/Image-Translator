87fbb5a35659b8eae4892287ed205e5e0761b4b2
SeHaziq <seh.haziqf@gmail.com>
2026-07-08 16:47:27 +0800

    create model for upload response

--- backend/app/api/upload.py
+++ backend/app/api/upload.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, UploadFile, File
-
+from app.models.upload_model import UploadResponse
 from app.services.upload_service import save_file
 
 router = APIRouter()
 
-
-@router.post("/upload")
+@router.post("/upload", response_model=UploadResponse)
 async def upload_image(file: UploadFile = File(...)):
     result = save_file(file)
     return result
